0cb60ba56f86172a9ff5f7e672af364bd7369989
Oleh Vaskevych <oleh.vaskevych@gmail.com>
2024-12-20 11:59:18 +0000

    Update settings.py

--- DjangoGramm/DjangoGramm/settings.py
+++ DjangoGramm/DjangoGramm/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["54.220.247.247", "localhost"]
+ALLOWED_HOSTS = ["54.171.140.119", "localhost"]
 
 DEFAULT_AVATAR_PATH = env("DEFAULT_AVATAR_PATH", default="avatars/default_avatar.jpg")
 
