9651765f0afaf167ef279a1d4e3789efd86f4a4a
Oleh Vaskevyh <oleh.vaskevych@gmail.com>
2024-12-20 13:56:27 +0200

    settings updated

--- DjangoGramm/DjangoGramm/urls.py
+++ DjangoGramm/DjangoGramm/urls.py
@@ -19,13 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls.static import static # new
 
 
 urlpatterns = [
     path('', include('social.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
