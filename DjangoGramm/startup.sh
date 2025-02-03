8b9f60efa7832221be0676ae0334156752651c20
Oleh Vaskevyh <oleh.vaskevych@gmail.com>
2025-02-03 12:48:47 +0200

    FIX: Added startup commands

--- DjangoGramm/startup.sh
+++ DjangoGramm/startup.sh
@@ -1,2 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+echo "Running post-deployment script..."
+
+# Активація віртуального середовища, якщо потрібно
+if [ -d "antenv" ]; then
+    source antenv/bin/activate
+fi
+
+# Встановлення бібліотек
+pip install --no-cache-dir -r requirements.txt
+
+python manage.py makemigrations
+# Виконання міграцій
+python manage.py migrate
+
+# Збір статичних файлів
+python manage.py collectstatic --noinput
+
+echo "Post-deployment script completed."
+
 gunicorn --workers=3 --bind=0.0.0.0:$PORT myproject.wsgi
